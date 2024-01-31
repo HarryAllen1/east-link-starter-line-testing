72b19ec37b1ce86896a08fd8a0827e3470a7e6a8
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2024-01-31 07:54:39 -0800

    change target

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [enhancedImages(), svelte()],
+  build: {
+    target: 'es2022',
+  },
 });
